5fd058df3f443ce873164a7946b2ccac0ab3c1d9
Daniel Aizcorbe <daizcorbe@frba.unt.edu.ar>
2022-04-06 23:35:17 -0300

    pseudocodigo macowins pero en formato wollok

---
+++ macowins.java
@@ -0,0 +1,95 @@
+object macowins {
+
+	var ventas = [];
+
+	method determinarGananciasDeUnDia(unDia) {
+		const ventasDelDia = ventas.filter{ venta => fueVendidoElDia(venta,unDia)};
+		return ventasDelDia.sum{ venta => venta.precioFinalDeLaVenta()};
+	}
+
+	method registrarVenta(unaVenta) {
+		ventas.add(unaVenta);
+	}
+}
+
+class Venta {
+
+	const prendasVendidas = [unasPrendas];
+	const fechaVenta = new Date();
+	const metodoDePago = unMetodo;
+
+	method consultarPrecioDePrendas() { 
+		return prendasVendidas.sum{ prendaVendida => prendaVendida.calcularPrecio()}
+}
+	
+	method precioFinalDeLaVenta() = self.consultarPrecioDePrendas() + metodoDePago.aplicarRecargo(self.consultarPrecioDePrendas())
+
+	method cantidadVendidas() = rendasVendidas.size()
+
+}
+
+class EstadoClase{
+	method calcularPrecio(precioBase);
+}
+
+object nueva {
+
+	method calcularPrecio(precioBase) {
+		return precioBase;
+	}
+
+}
+
+class Promocion inherits EstadoPrenda{
+
+	const valorFijo;
+
+	method calcularPrecio(precioBase) {
+		return precioBase - valorFijo;
+	}
+
+}
+
+object liquidacion {
+
+	method calcularPrecio(precioBase) {
+		return precioBase * 0.5;
+	}
+}
+
+class MetodoDePago {
+
+	method aplicarRecargo(){}
+
+}
+
+class Tarjeta inherits MetodoDePago {
+
+	var property cuotas
+	const coeficienteFijo
+
+	method aplicarRecargo(preciosPrendasVendidas) {
+		return cuotas * coeficienteFijo + preciosPrendasVendidas * 0.01
+	}
+}
+
+object efectivo {
+
+		method aplicarRecargo(preciosPrendasVendidas){ 
+			return preciosPrendasVendidas;
+	}
+}
+
+class Prenda {
+
+	const precioBase
+	const tipoDePrenda
+	var estadoPrenda
+
+	method calcularPrecio() {
+		return estadoPrenda.calcularPrecio(precioBase)
+	}
+
+	method tipoDePrenda() = tipoDePrenda;
+
+}
